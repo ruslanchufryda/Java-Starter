a4843f42ec906d949e9eed641997be037085f362
Ruslan Chufryda <rulya987654321@gmail.com>
2019-05-24 14:08:18 +0300

    дописано пункт

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1.Проснутись\n" +
                 "2.Почистити зуби\n" +
-                "3.Випити каву\n");
+                "3.Випити каву\n"+
+                "4.Одіти шапку");
     }
 }
